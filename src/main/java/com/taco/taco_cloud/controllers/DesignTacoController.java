d1c145919e59641c77da2f8e4aa0bc8e2f11c250
Myatty <myintmyatps664@gmail.com>
2025-03-29 15:26:45 +0630

    add ingredient by id converter

--- src/main/java/com/taco/taco_cloud/controllers/DesignTacoController.java
+++ src/main/java/com/taco/taco_cloud/controllers/DesignTacoController.java
@@ -6,19 +6,18 @@ import java.util.stream.Collectors;
 import com.taco.taco_cloud.models.TacoOrder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 import lombok.extern.slf4j.Slf4j;
 import com.taco.taco_cloud.models.Ingredient;
 import com.taco.taco_cloud.models.Ingredient.Type;
 import com.taco.taco_cloud.models.Taco;
+
 @Slf4j
 @Controller
 @RequestMapping("/design")
 @SessionAttributes("tacoOrder")
 public class DesignTacoController {
+
     @ModelAttribute
     public void addIngredientsToModel(Model model) {
         List<Ingredient> ingredients = Arrays.asList(
@@ -39,23 +38,37 @@ public class DesignTacoController {
                     filterByType(ingredients, type));
         }
     }
+
     @ModelAttribute(name = "tacoOrder")
     public TacoOrder order() {
         return new TacoOrder();
     }
+
     @ModelAttribute(name = "taco")
     public Taco taco() {
         return new Taco();
     }
+
     @GetMapping
     public String showDesignForm() {
         return "design";
     }
+
     private Iterable<Ingredient> filterByType(
             List<Ingredient> ingredients, Type type) {
         return ingredients
                 .stream()
                 .filter(x -> x.getType().equals(type))
                 .collect(Collectors.toList());
+    }
+
+    @PostMapping
+    public String processTaco(Taco taco,
+                              @ModelAttribute TacoOrder tacoOrder){
+        tacoOrder.addTaco(taco);
+        log.info("Processing Taco: {}", taco);
+
+        return "redirect:/orders/current";
+
     }
 }
--- src/main/java/com/taco/taco_cloud/models/Taco.java
+++ src/main/java/com/taco/taco_cloud/models/Taco.java
@@ -8,6 +8,5 @@ import java.util.List;
 public class Taco {
 
     private String name;
-
     private List<Ingredient> ingredients;
 }
