dd6862e7ca89c25196b41fd6d73cd214bc3f8cc1
Myatty <myintmyatps664@gmail.com>
2025-03-29 16:30:19 +0630

    validate the taco order and test validation

--- src/main/java/com/taco/taco_cloud/controllers/OrderController.java
+++ src/main/java/com/taco/taco_cloud/controllers/OrderController.java
@@ -1,12 +1,12 @@
 package com.taco.taco_cloud.controllers;
+import com.taco.taco_cloud.models.Taco;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 import lombok.extern.slf4j.Slf4j;
 import com.taco.taco_cloud.models.TacoOrder;
+import jakarta.validation.Valid;
+import org.springframework.validation.Errors;
 
 @Slf4j
 @Controller
@@ -20,8 +20,11 @@ public class OrderController {
     }
 
     @PostMapping
-    public String processOrder(TacoOrder order,
+    public String processOrder(@Valid TacoOrder order, Errors errors,
                                SessionStatus sessionStatus) {
+        if (errors.hasErrors()) {
+            return "orderForm";
+        }
         log.info("Order submitted: {}", order);
         sessionStatus.setComplete();
         return "redirect:/";
