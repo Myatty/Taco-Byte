a67db2d80cd1f3401c2e46c6085108d0d9df5693
Myatty <myintmyatps664@gmail.com>
2025-04-01 22:55:52 +0630

    fix query

--- src/main/java/com/taco/taco_cloud/repositories/JdbcOrderRepository.java
+++ src/main/java/com/taco/taco_cloud/repositories/JdbcOrderRepository.java
@@ -30,7 +30,7 @@ public class JdbcOrderRepository implements OrderRepository {
     public TacoOrder save(TacoOrder order) {
         PreparedStatementCreatorFactory pscf =
                 new PreparedStatementCreatorFactory(
-                        "INSERT INTO Taco_Order "
+                        "insert into Taco_Order "
                                 + "(delivery_name, delivery_street, delivery_city, "
                                 + "delivery_state, delivery_zip, cc_number, "
                                 + "cc_expiration, cc_cvv, placed_at) "
