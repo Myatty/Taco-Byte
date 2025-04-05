1507507f7d7953b0d7a4b023951f265168cadc69
Myatty <myintmyatps664@gmail.com>
2025-04-05 14:20:18 +0630

    extend repository in ingredientRepo

--- src/main/java/com/taco/taco_cloud/repositories/IngredientRepository.java
+++ src/main/java/com/taco/taco_cloud/repositories/IngredientRepository.java
@@ -1,10 +1,11 @@
 package com.taco.taco_cloud.repositories;
 
 import com.taco.taco_cloud.models.Ingredient;
+import org.springframework.data.repository.Repository;
 
 import java.util.Optional;
 
-public interface IngredientRepository {
+public interface IngredientRepository extends Repository<Ingredient, String> {
 
     Iterable<Ingredient> findAll();
     Optional<Ingredient> findById(String id);
