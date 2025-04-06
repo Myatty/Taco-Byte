17da3df8789da845789aa09d0cb7c9763f1c0a64
Myatty <myintmyatps664@gmail.com>
2025-04-06 21:38:12 +0630

    chore: remove unused import and change from String to Long in OrderRepo Generic

--- src/main/java/com/taco/taco_cloud/repositories/IngredientRepository.java
+++ src/main/java/com/taco/taco_cloud/repositories/IngredientRepository.java
@@ -3,8 +3,6 @@ package com.taco.taco_cloud.repositories;
 import com.taco.taco_cloud.models.Ingredient;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
-
 public interface IngredientRepository extends CrudRepository<Ingredient, String> {
 
 }
--- src/main/java/com/taco/taco_cloud/repositories/OrderRepository.java
+++ src/main/java/com/taco/taco_cloud/repositories/OrderRepository.java
@@ -3,6 +3,6 @@ package com.taco.taco_cloud.repositories;
 import com.taco.taco_cloud.models.TacoOrder;
 import org.springframework.data.repository.CrudRepository;
 
-public interface OrderRepository extends CrudRepository<TacoOrder, String> {
+public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
 
 }
