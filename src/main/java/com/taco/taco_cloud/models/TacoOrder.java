9b94b5a7912bff1ddccf3715b3cfe543648a1e8e
Myatty <myintmyatps664@gmail.com>
2025-04-06 21:32:08 +0630

    refactor tacoOrder according to JPA

--- src/main/java/com/taco/taco_cloud/models/TacoOrder.java
+++ src/main/java/com/taco/taco_cloud/models/TacoOrder.java
@@ -3,25 +3,32 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
 import java.util.ArrayList;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
 import lombok.Data;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import org.hibernate.validator.constraints.CreditCardNumber;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
-import org.springframework.data.relational.core.mapping.Table;
 
 @Data
-@Table("Taco_Byte_Order")
+@Entity
 public class TacoOrder implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    private Date placedAt;
+    private Date placedAt = new Date();
 
     @Column("customer_name")
     @NotBlank(message="Delivery name is required")
@@ -49,6 +56,15 @@ public class TacoOrder implements Serializable {
     @Digits(integer=3, fraction=0, message="Invalid CVV")
     private String ccCVV;
 
+    /*
+
+    An order can contain multiple tacos,
+    and these tacos are associated with this specific order.
+
+    If order is deleted, all related tacos are also deleted (CascadeType.ALL).
+
+     */
+    @OneToMany(cascade = CascadeType.ALL)
     private List<Taco> tacos = new ArrayList<>();
 
     public void addTaco(Taco taco) {
